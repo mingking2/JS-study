b3116bdcbb73de63d9b3ea6db533f0be9e232f7b
King's meow <mingee641@gmail.com>
2023-05-31 17:49:24 +0900

    today 출력부 수정 및 Map() 사용

--- week 5/mingi5.js
+++ week 5/mingi5.js
@@ -5,64 +5,65 @@ const today = document.getElementById('today');
 const submit = document.getElementById('submit');
 const answer = document.getElementById('answer');
 
+const total = new Date();
+const year = total.getFullYear();
+const month = total.getMonth() + 1;
+const date = total.getDate();
+const todayDate = year + "-" + month + "-" + date;
+today.innerHTML += todayDate;
 
-const showToday = () => {
-    const total = new Date();
-    const year = total.getFullYear();
-    const month = total.getMonth() + 1;
-    const date = total.getDate();
 
-    today.innerHTML += year + "-" + month + "-" + date;
-}
 
 submit.addEventListener('click', () => {
-    const info = {
-        name: name_.value,
-        birthday: birthday.value,
-        gender: gender.value,
-        today: today.innerText.substring(6)
-    }
+    const info = new Map();
 
+    info.set(name_,name_.value);
+    info.set(birthday,birthday.value)
+    info.set(gender,gender.value)
+    info.set(today,todayDate)
+
+    if (!info.get(name_) || !info.get(birthday) || !info.get(gender) || !info.get(today)) {
+        alert("데이터가 비었습니다.");
+        return;
+    }
     console.log(info);
 
-     // 행운의 숫자 계산
-     const luckyNumber = calculateLuckyNumber(info);
+    // 행운의 숫자 계산
+    const luckyNumber = calculateLuckyNumber(info);
 
-     // 결과 표시
-     answer.innerHTML = `Your lucky number is: ${luckyNumber}`;
+    // 결과 표시
+    answer.innerHTML = `Your lucky number is: ${luckyNumber}`;
 });
 
 const calculateLuckyNumber = (info) => {
-     // 각 정보를 이용하여 행운의 숫자 계산
-     let luckyNumber = 0;
+    // 각 정보를 이용하여 행운의 숫자 계산
+    let luckyNumber = 0;
 
-     // 이름 길이에 따른 변동값 추가
-     luckyNumber += info.name.length;
- 
-     // 생년월일의 각 자리수를 합하여 변동값 추가
-     const birthDate = new Date(info.birthday);
-     const birthYear = birthDate.getFullYear();
-     const birthMonth = birthDate.getMonth() + 1;
-     const birthDay = birthDate.getDate();
-     const birthSum = birthYear + birthMonth + birthDay;
-     luckyNumber += sumDigits(birthSum);
- 
-     // 성별에 따른 변동값 추가
-     if (info.gender === 'male') {
-         luckyNumber += 5;
-     } else if (info.gender === 'female') {
-         luckyNumber += 10;
-     }
- 
-     // 오늘 날짜의 각 자리수를 합하여 변동값 추가
-     const todayDate = new Date(info.today);
-     const todayYear = todayDate.getFullYear();
-     const todayMonth = todayDate.getMonth() + 1;
-     const todayDay = todayDate.getDate();
-     const todaySum = todayYear + todayMonth + todayDay;
-     luckyNumber += sumDigits(todaySum);
- 
-     return luckyNumber;
+    // 이름 길이에 따른 변동값 추가
+    const select_name = info.get(name_);
+    const foundValue = Array.from(info.values()).find(value => value === select_name);
+    luckyNumber += foundValue.length;
+    console.log(foundValue.length);
+    // 생년월일의 각 자리수를 합하여 변동값 추가
+    const birthDate = new Date(info.get(birthday));
+    const birthYear = birthDate.getFullYear();
+    const birthMonth = birthDate.getMonth() + 1;
+    const birthDay = birthDate.getDate();
+    const birthSum = birthYear + birthMonth + birthDay;
+    luckyNumber += sumDigits(birthSum);
+
+    // 성별에 따른 변동값 추가
+    if (info.get(gender) === 'male') {
+        luckyNumber += 5;
+    } else {
+        luckyNumber += 10;
+    }
+
+    // 오늘 날짜의 각 자리수를 합하여 변동값 추가
+    const todaySum = year + month + date;
+    luckyNumber += sumDigits(todaySum);
+
+    return luckyNumber;
 }
 
 const sumDigits = (number) => {
@@ -73,5 +74,3 @@ const sumDigits = (number) => {
     }
     return sum;
 }
-
-showToday();
