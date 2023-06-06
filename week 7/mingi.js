d515ce713a4c8a7ffe031197b50718eeff2a70af
King's meow <mingee641@gmail.com>
2023-06-06 18:12:28 +0900

    clock 기능 구현

---
+++ week 7/mingi.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector('.todo-title');
+
+const getTime = () => {
+    const chtime = new Date();
+    const hours = chtime.getHours();
+    const minutes = chtime.getMinutes();
+    const seconds = chtime.getSeconds();
+    clock.innerHTML = `${hours<10 ? `0${hours}`:hours}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`;
+
+}
+
+setInterval(getTime, 1000);
